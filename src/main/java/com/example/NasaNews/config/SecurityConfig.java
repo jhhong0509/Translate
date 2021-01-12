f62f5867b9a65d0c362e7911226e2e71b8f7905d
Hong Jung hyeon <jhhong0509@naver.com>
2021-01-12 16:04:50 +0900

    update

--- src/main/java/com/example/NasaNews/config/SecurityConfig.java
+++ src/main/java/com/example/NasaNews/config/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.example.NasaNews.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;z
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
